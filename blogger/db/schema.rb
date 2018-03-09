324f3be34b315e5316b9da178106d0c27073d24e
mikkel250 <mikkel250@gmail.com>
2018-03-08 16:37:48 -0800

    Adding some articles, edit routes.rb file

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 20180308060614) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
